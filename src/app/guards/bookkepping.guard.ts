dee38f9e4fd86d601a3946fc7521c9afadda9c49
Larax1011 <laza98@gmail.com>
2022-03-26 21:43:15 +0100

    Bugfix guards

--- src/app/guards/bookkepping.guard.ts
+++ src/app/guards/bookkepping.guard.ts
@@ -2,27 +2,29 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import {UserService} from "../services/user.service";
+import {User} from "../../model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookkeppingGuard implements CanActivate {
-  constructor(private userService: UserService) {
+  user!: User;
 
+
+  constructor(private userService: UserService) {
+    this.userService.getLoggedInUser().subscribe(user => {
+      this.user = user
+    })
   }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'knjizenje')
-          return true;
-      }
-      return false;
-    })
-    return false;
+    for (let i = 0; i < this.user.authorities.length; i++) {
+      if (this.user.authorities[i].name === 'knjizenje')
+        return true
+    }
+    return false
   }
 
 }
