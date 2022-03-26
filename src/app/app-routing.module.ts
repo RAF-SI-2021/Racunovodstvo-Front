8a72e58039891507783b588c0d382bce174a350f
Larax1011 <laza98@gmail.com>
2022-03-26 18:27:10 +0100

    Login routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
 import {ManageUsersComponent} from "../manage-users/manage-users.component";
 
 const routes: Routes = [
   {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "",
+    component: LoginComponent,
+  },
+  {
     path: "manage-users",
     component: ManageUsersComponent,
     canActivate: []
-  }
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule { }
