3dc90ef3852f1bb3b9f99e2725e93df117469c2f
Larax1011 <laza98@gmail.com>
2022-03-26 18:36:11 +0100

    Page guards

---
+++ src/app/guards/evidencije.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import {UserService} from "../services/user.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EvidencijeGuard implements CanActivate {
+  constructor(private userService: UserService) {
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    this.userService.getPermissions().subscribe(permissions => {
+      console.log(permissions)
+      for (let i = 0; i < permissions.length; i++) {
+        if (permissions[i].name.toLowerCase() == 'evidencije')
+          return true;
+      }
+      return false;
+    })
+    return false;
+  }
+
+}
