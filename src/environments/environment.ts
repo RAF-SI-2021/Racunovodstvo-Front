7c4941097e00ca151741b0723e7737c72ed98314
Larax1011 <laza98@gmail.com>
2022-03-26 21:45:04 +0100

    Added user api path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   authApi: 'http://localhost:8080/auth',
-  permissionApi: 'http://localhost:8080/api/permissions'
+  permissionApi: 'http://localhost:8080/api/permissions',
+  userApi: 'http://localhost:8080/api/users'
 };
 
 /*
