27b840f22a4634209ac88f8ea82492f49d24785f
Larax1011 <laza98@gmail.com>
2022-03-26 19:53:40 +0100

    Fixed permissions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 import {UserService} from "./services/user.service";
+import {Permission} from "../model";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import {UserService} from "./services/user.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  permissions: Permission[] = []
   title = 'racunovodstvo';
 
   constructor(private router: Router, private userService: UserService) {
@@ -15,179 +17,169 @@ export class AppComponent {
   }
 
 
-
   isAdmin(): boolean {
     //TODO is this correct for admin?
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'admin')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'admin')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   loggedIn(): boolean {
-  return localStorage.getItem('jwt') != null
+    if (this.permissions.length == 0 && localStorage.getItem('jwt') != null) {
+      this.userService.getPermissions().subscribe(permissions => {
+        this.permissions = permissions
+      })
+    }
+    return localStorage.getItem('jwt') != null
   }
 
   logout() {
     localStorage.removeItem('jwt')
+    this.permissions = []
     this.router.navigate(['login'])
   }
 
   canProfile(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'profil')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'profil')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canEvidence(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'evidencija')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'evidencija')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canAcquisitions(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'nabavke')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'nabavke')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canSales(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'prodaja')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'prodaja')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canReports(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'izvestaji')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'izvestaji')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canBookkeeping(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'knjizenje')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'knjizenje')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canAccountPlan(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'kontni plan')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'kontni plan')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canBookkeepingJournal(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'dnevnik knjizenja')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'dnevnik knjizenja')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canMainBook(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'glavna knjiga')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'glavna knjiga')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canKUF(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'kuf')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'kuf')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canKIF(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'kif')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'kif')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canAddNewInvoice(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'dodaj fakturu')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'dodaj fakturu')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canAddNewClient(): boolean {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'dodaj komitenta')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'dodaj komitenta')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 
   canObracunZarade() {
-    this.userService.getPermissions().subscribe(permissions => {
-      for (let i = 0; i < permissions.length; i++) {
-        if (permissions[i].name.toLowerCase() == 'obracun zarade')
+    if (this.loggedIn()) {
+      for (let i = 0; i < this.permissions.length; i++) {
+        if (this.permissions[i].name.toLowerCase() == 'obracun zarade')
           return true;
       }
-      return false;
-    })
+    }
     return false;
   }
 }
