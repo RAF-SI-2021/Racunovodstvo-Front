3c6ee7ca29a9694069ba5d4bca9044ce5a6a08fa
Larax1011 <laza98@gmail.com>
2022-03-26 21:45:04 +0100

    Added user api path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   add_upd_del_user: "http://localhost:8080/api/users/",
   list_users: "http://localhost:8080/api/users/all",
   authApi: 'http://localhost:8080/auth',
-  permissionApi: 'http://localhost:8080/api/permissions'
+  permissionApi: 'http://localhost:8080/api/permissions',
+  userApi: 'http://localhost:8080/api/users'
 };
 
 /*
