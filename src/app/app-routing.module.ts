1b6d9e97661b6ef5f64737374ac8ccb9834b8ade
Larax1011 <laza98@gmail.com>
2022-03-26 19:54:09 +0100

    Added temp profile landing page and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
+import {ProfileTestComponent} from "./components/profile-test/profile-test.component";
 
 const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent,
+  },
   {
     path: "login",
     component: LoginComponent,
   },
   {
-    path: "",
-    component: LoginComponent,
+    path: "profile",
+    component: ProfileTestComponent,
   },
 ];
 
