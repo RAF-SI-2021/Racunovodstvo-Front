9b978d4f591b9062bd03aa4dac2642594cf59cf1
Larax1011 <laza98@gmail.com>
2022-03-26 18:39:33 +0100

    User service, get permissions

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Permission} from "../../model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly apiUrl = environment.permissionApi
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPermissions(): Observable<Permission[]> {
+    let url = `${this.apiUrl}/all`
+    return this.httpClient.get<Permission[]>(url)
+  }
+}
