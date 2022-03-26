4d792af9b319031aabe381f534b6d3d99874d96d
Larax1011 <laza98@gmail.com>
2022-03-26 18:38:53 +0100

    Login service, implemented login

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {LoginResponse} from "../../model";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private readonly apiUrl = environment.authApi
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(email: string, password: string): Observable<HttpResponse<LoginResponse>> {
+    let body =
+      {
+        email: email,
+        password: password
+      }
+    let url = `${this.apiUrl}/login`
+    return this.httpClient.post<LoginResponse>(url, body, {observe: 'response'})
+  }
+
+
+}
