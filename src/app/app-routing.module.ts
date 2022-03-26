d7df3cbd4c48cc7e6018d970b90b82724266cdcd
Larax1011 <laza98@gmail.com>
2022-03-26 21:44:49 +0100

    added guard for profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {ProfileTestComponent} from "./components/profile-test/profile-test.component";
+import {ProfileGuard} from "./guards/profile.guard";
 
 const routes: Routes = [
   {
@@ -15,6 +16,7 @@ const routes: Routes = [
   {
     path: "profile",
     component: ProfileTestComponent,
+    canActivate: [ProfileGuard]
   },
 ];
 
