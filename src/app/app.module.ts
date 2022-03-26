f49e677b3f1a88b5403eb11b0abe443023ba326f
Larax1011 <laza98@gmail.com>
2022-03-26 19:54:33 +0100

    Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { LoginComponent } from './components/login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import { ProfileTestComponent } from './components/profile-test/profile-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProfileTestComponent
   ],
   imports: [
     BrowserModule,
