14dcd09555745b8125f24b1c938eb3c04900bd78
Larax1011 <laza98@gmail.com>
2022-03-26 18:46:39 +0100

    Test jwt comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   title = 'racunovodstvo';
 
   constructor(private router: Router, private userService: UserService) {
+    // localStorage.setItem('jwt', 'testjwt')
   }
 
 
