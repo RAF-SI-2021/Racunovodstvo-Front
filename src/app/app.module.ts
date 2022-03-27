73f60fc1e65878a0432d5913654f1aaac20141fc
dejankulic <73189801+dejankulic@users.noreply.github.com>
2022-03-28 01:08:59 +0200

    commit (#19)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddNewInvoiceComponent } from './add-new-invoice/add-new-invoice.component';
+import { ReactiveFormsModule, FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddNewInvoiceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
