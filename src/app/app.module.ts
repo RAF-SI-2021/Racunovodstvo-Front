c0368e7dc5d221304eb8c2047a5d5863c85b5833
Larax1011 <laza98@gmail.com>
2022-03-29 21:30:37 +0200

    Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { ProfileTestComponent } from './components/profile-test/profile-test.component';
+import { AccountPlanComponent } from './components/account-plan/account-plan.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ProfileTestComponent
+    ProfileTestComponent,
+    AccountPlanComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +25,8 @@ import { ProfileTestComponent } from './components/profile-test/profile-test.com
     NgbModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MatTableModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
