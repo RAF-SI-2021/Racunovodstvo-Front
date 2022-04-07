1d6d047359890364b5eacc7240a40e78dd8b7b4c
boris.stojanovic <boris.stojanovic@msg-global.com>
2022-04-07 21:37:24 +0200

    feature/docker: Dodao Dockerfile, dodao putanje u environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
 	production: true,
+
+  APIEndpoint: 'http://localhost:8080',
+
+  add_upd_del_user: 'http://localhost:8080/api/users/',
+  list_users: 'http://localhost:8080/api/users/all',
+
+  authApi: 'http://localhost:8080/auth',
+  permissionApi: 'http://localhost:8080/api/permissions',
+  userApi: 'http://localhost:8080/api/users',
 };
