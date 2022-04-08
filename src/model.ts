c4599c7cdf335788296d31a0c06aa3f2155e2a1c
Nikola <nboskovic11019rn@raf.rs>
2022-04-08 16:22:06 +0200

    Dnevnik knjizenja

--- src/model.ts
+++ src/model.ts
@@ -34,3 +34,47 @@ export class Permission{
   }
 }
 
+export interface DnevnikKnjizenja{
+  knjizenjeId: number;
+  brojNaloga: string;
+  datumKnjizenja: Date;
+  duguje: number;
+  potrazuje: number;
+  dokumentId:number;
+  saldo: number;
+  komentar: string;
+}
+
+export class DnevnikKnjizenja{
+  knjizenjeId: number;
+  brojNaloga: string;
+  datumKnjizenja: Date;
+  duguje: number;
+  potrazuje: number;
+  dokumentId:number;
+  saldo: number;
+  komentar: string;
+
+  constructor(
+    knjizenjeId: number,
+    brojNaloga: string,
+    datumKnjizenja: Date,
+    duguje: number,
+    potrazuje: number,
+    dokumentId:number,
+    saldo: number,
+    komentar: string
+  ) {
+    this.knjizenjeId = knjizenjeId;
+    this.datumKnjizenja = datumKnjizenja;
+    this.brojNaloga = brojNaloga;
+    this.duguje = duguje;
+    this.potrazuje = potrazuje;
+    this.dokumentId = dokumentId;
+    this.saldo = saldo;
+    this.komentar = komentar;
+  }
+}
+
+
+
