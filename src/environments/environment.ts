b374ac2265967785ef935aa6bdf6d2ea185ab0d6
Larax1011 <laza98@gmail.com>
2022-05-11 16:41:33 +0200

    izvestaj environment api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,6 +16,7 @@ export const environment = {
 
 	kontnaGrupaApi: 'http://localhost:8080/api/konto',
 	brutoBilansApi: 'http://localhost:8080/api/bilans',
+  izvestajiApi: 'http://localhost:8080/api/izvestaji',
 };
 
 /*
