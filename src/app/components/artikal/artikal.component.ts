e0a611693d749de12047c215d74e215618a7fec7
Nikola <nboskovic11019rn@raf.rs>
2022-06-05 12:53:17 +0200

    Gotove evidencije. Testirano i radi sve TREBA DA SE DODA GARD ZA PRISTUP.

--- src/app/components/artikal/artikal.component.ts
+++ src/app/components/artikal/artikal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, UrlTree} from "@angular/router";
-import {Artikal} from "../../shared/konverzija.model";
+import {Artikal, Istorija} from "../../shared/konverzija.model";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {KonverzijaService} from "../../services/konverzija/konverzija.service";
 
@@ -12,6 +12,8 @@ import {KonverzijaService} from "../../services/konverzija/konverzija.service";
 export class ArtikalComponent implements OnInit {
 
   artikal: FormGroup;
+  artikal2 = {} as Artikal;
+  istorija: Istorija[] = [];
 
   constructor(private route:Router, private formBuilder: FormBuilder, private artikalService: KonverzijaService) {
     this.artikal = this.formBuilder.group({
@@ -20,26 +22,64 @@ export class ArtikalComponent implements OnInit {
       jedinicaMere: ['C'],
       kolicina: ['D'],
       nabavnaCena: ['E'],
+      prodajnaCena:[''],
       rabatProcenat: ['F'],
       rabat: ['G'],
       nabavnaCenaPosleRabata: ['H'],
-      ukupnaNabavnaVrednost: ['I']
+      ukupnaNabavnaVrednost: ['I'],
+      istorijaProdajneCene: [[]],
+      konverzijaKalkulacijaId: ['']
     });
-
   }
 
   ngOnInit(): void {
 
-    // TODO uradi getArtikal sa tim idem da se prikaze sve za njega
-    // alert(this.route.url.substring(this.route.url.lastIndexOf('/') + 1))
-    // this.artikalService.getArtikli(parseInt(this.route.url.substring(this.route.url.lastIndexOf('/') + 1))).subscribe(artikal=>{
-    //   this.artikal = artikal.content
-    //   // this.addingFormArtikal.setValue({rabat: this.addingFormArtikal.get('nabavnaCena') * (this.addingFormArtikal.get('rabatProcenat') / 100)})
-    //
-    // })
+    let artikalID = this.route.url.substring(this.route.url.lastIndexOf('/') + 1)
+
+    this.artikalService.getSviArtikli().subscribe(artikli=>{
+      let niz = artikli.content
+      for(let i =0; i< niz.length; i++){
+        if(niz[i].artikalId == Number(artikalID)){
+          let izNiza = niz[i];
+          this.artikal2 = niz[i]
+          this.artikal.get('sifraArtikla')?.setValue(izNiza.sifraArtikla);
+          this.artikal.get('nazivArtikla')?.setValue(izNiza.nazivArtikla);
+          this.artikal.get('jedinicaMere')?.setValue(izNiza.jedinicaMere);
+          this.artikal.get('sifraArtikla')?.setValue(izNiza.sifraArtikla);
+          this.artikal.get('kolicina')?.setValue(izNiza.kolicina);
+          this.artikal.get('nabavnaCena')?.setValue(izNiza.nabavnaCena);
+          this.artikal.get('rabatProcenat')?.setValue(izNiza.rabatProcenat);
+          this.artikal.get('rabat')?.setValue(izNiza.rabat);
+          this.artikal.get('nabavnaCenaPosleRabata')?.setValue(izNiza.nabavnaCenaPosleRabata);
+          this.artikal.get('ukupnaNabavnaVrednost')?.setValue(izNiza.ukupnaNabavnaVrednost);
+          this.artikal.get('istorijaProdajneCene')?.setValue(izNiza.istorijaProdajneCene);
+          this.artikal.get('prodajnaCena')?.setValue(izNiza.prodajnaCena);
+          this.artikal.get('konverzijaKalkulacijaId')?.setValue(izNiza.konverzijaKalkulacijaId)
+          this.istorija = this.artikal.get('istorijaProdajneCene')?.value
+          console.log(this.istorija)
+        }
+      }
+
+    })
   }
 
   updateCena() {
-
+    this.artikal2.prodajnaCena = this.artikal.get('prodajnaCena')?.value
+    this.artikalService.updateProdajnaCena(this.artikal2).subscribe(artikal =>{
+      let izNiza = artikal;
+      this.artikal.get('sifraArtikla')?.setValue(izNiza.sifraArtikla);
+      this.artikal.get('nazivArtikla')?.setValue(izNiza.nazivArtikla);
+      this.artikal.get('jedinicaMere')?.setValue(izNiza.jedinicaMere);
+      this.artikal.get('sifraArtikla')?.setValue(izNiza.sifraArtikla);
+      this.artikal.get('kolicina')?.setValue(izNiza.kolicina);
+      this.artikal.get('nabavnaCena')?.setValue(izNiza.nabavnaCena);
+      this.artikal.get('rabatProcenat')?.setValue(izNiza.rabatProcenat);
+      this.artikal.get('rabat')?.setValue(izNiza.rabat);
+      this.artikal.get('nabavnaCenaPosleRabata')?.setValue(izNiza.nabavnaCenaPosleRabata);
+      this.artikal.get('ukupnaNabavnaVrednost')?.setValue(izNiza.ukupnaNabavnaVrednost);
+      this.artikal.get('istorijaProdajneCene')?.setValue(izNiza.istorijaProdajneCene);
+      this.artikal.get('prodajnaCena')?.setValue(izNiza.prodajnaCena);
+      this.istorija = this.artikal.get('istorijaProdajneCene')?.value
+    })
   }
 }
--- src/app/components/evidencije/evidencije.component.ts
+++ src/app/components/evidencije/evidencije.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Artikal} from "../../shared/konverzija.model";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {Router} from "@angular/router";
@@ -13,7 +13,7 @@ export class EvidencijeComponent implements OnInit {
   artikli: Artikal[] = [];
   filterForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private route:Router, private konverzijaService: KonverzijaService) {
+  constructor(private formBuilder: FormBuilder, private route: Router, private konverzijaService: KonverzijaService) {
     this.filterForm = this.formBuilder.group({
       sifraArtikla: [''],
       nazivArtikla: [''],
@@ -23,16 +23,24 @@ export class EvidencijeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.konverzijaService.getArtikli(1).subscribe(artikli =>{
+    this.konverzijaService.getSviArtikli().subscribe(artikli => {
       this.artikli = artikli.content;
     });
   }
 
   prikaziArtikal(id: number) {
-    this.route.navigate(['/artikal/'+id]);
+    this.route.navigate(['/artikal/' + id]);
   }
 
+  // sifraArtikla: string,     nazivArtikla: string,     jedinicaMere: string,     kolicina
   pretrazi() {
-
+    this.konverzijaService.pretrazi(this.filterForm.get('sifraArtikla')?.value, this.filterForm.get('nazivArtikla')?.value,
+      this.filterForm.get('jedinicaMere')?.value, this.filterForm.get('kolicina')?.value).subscribe(pageable => {
+      this.artikli = pageable.content;
+      this.filterForm.get('jedinicaMere')?.setValue('');
+      this.filterForm.get('sifraArtikla')?.setValue('');
+      this.filterForm.get('nazivArtikla')?.setValue('');
+      this.filterForm.get('kolicina')?.setValue('');
+    })
   }
 }
