b4810c04ebfcf68b4a79374955c911697005646f
MilenaDimitrijevic <mdimitrijevic11419rn@raf.rs>
2022-06-12 17:12:57 +0200

    environment.prod.ts dodatak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
 	production: true,
 
   APIEndpoint: 'http://localhost:8080',
+  APIEndpoint1: 'http://localhost:8081',
 
   add_upd_del_user: 'http://localhost:8080/api/users/',
   list_users: 'http://localhost:8080/api/users/all',
@@ -12,4 +13,13 @@ export const environment = {
 
   kontnaGrupaApi: 'http://localhost:8080/api/konto',
   brutoBilansApi: 'http://localhost:8080/api/bilans',
+  izvestajiApi: 'http://localhost:8080/api/izvestaji',
+  analitkceKarticeApi: 'http://localhost:8080/api/knjizenje/analitickeKartice',
+
+  bilans_stanja: 'http://localhost:8080/api/izvestaji/stanje',
+  bilans_uspeha: 'http://localhost:8080/api/izvestaji/uspeh',
+
+  profitniCentriApi: 'http://localhost:8080/api/profitni-centri',
+
+  troskovni_centar: 'http://localhost:8080/api/troskovni-centri'
 };
