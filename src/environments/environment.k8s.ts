26dd4c91b862a51a54d2e2bab252ed695fb9f1e9
boris.stojanovic <boris.stojanovic@msg-global.com>
2022-06-13 10:35:09 +0200

    feature/k8s-setup: Popravio url za knjizenje

--- src/environments/environment.k8s.ts
+++ src/environments/environment.k8s.ts
@@ -1,6 +1,6 @@
 const userServiceApi = 'http://chrysus.raf/user-service';
 const nabavkaServiceApi = 'http://chrysus.raf/nabavka-service';
-const knjizenjeServiceApi = 'http://chrysus.raf//knjizenje-service';
+const knjizenjeServiceApi = 'http://chrysus.raf/knjizenje-service';
 const preduzeceServiceApi = 'http://chrysus.raf/preduzece-service';
 
 export const environment = {
