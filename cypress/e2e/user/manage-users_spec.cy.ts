de8c800112b41ff99efe33ec8803ba6d5079271a
Kosta <73071480+kmarkovic7019rn@users.noreply.github.com>
2022-07-02 23:16:56 +0200

    Testiranje (#107)

--- cypress/e2e/user/manage-users_spec.cy.ts
+++ cypress/e2e/user/manage-users_spec.cy.ts
@@ -3,7 +3,14 @@ describe('manage-users spec', () => {
 		cy.visit('/manage-users');
 	});
 
-	// kako dobiti permisije ?
+	it('dodaj novog', () => {
+		cy.get('.btn-success').click();
+		cy.get('#username2').type('user3');
+		cy.get('#InputName2').type('Ime');
+		cy.get('#InputSurname2').type('Prezime');
+		cy.get('#InputPassword').type('user1234');
+		cy.get('.create-account').click();
+	});
 
-	it('validation', () => {});
+	// testirati jos stvari
 });
